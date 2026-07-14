fae5d763db822c60479a48d25e9e42703398a927
Eduardo Vinícius <eduardovdsr@gmail.com>
2026-07-14 11:45:21 -0300

    feat: add interactive cube face

--- src/components/InteractiveCube/InteractiveCube.tsx
+++ src/components/InteractiveCube/InteractiveCube.tsx
@@ -1,6 +1,27 @@
+import { Html } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { FaFaceGrin } from "react-icons/fa6";
 import { Group } from "three";
+
+function CubeFace({ position, rotation }: { position: [number, number, number]; rotation: [number, number, number]; }) {
+    const [hovered, setHovered] = useState(false)
+
+    return (
+        <mesh
+            position={position}
+            rotation={rotation}
+            onPointerOver={(e) => { e.stopPropagation(); setHovered(true) }}
+            onPointerOut={(e) => { e.stopPropagation(); setHovered(false); }}>
+            <planeGeometry args={[3, 3]} />
+            <Html center occlude={false} style={{ pointerEvents: "none" }}>
+                <div style={{ opacity: hovered ? 1 : 0, transition: "opacity 0.3s" }}>
+                    <FaFaceGrin color="yellow" size={50} />
+                </div>
+            </Html>
+        </mesh>
+    )
+}
 
 function Cube() {
     const cube = useRef<Group | null>(null)
@@ -17,6 +38,7 @@ function Cube() {
                 <boxGeometry args={[3, 3, 3]} />
                 <meshStandardMaterial color="orange" />
             </mesh>
+            <CubeFace position={[0,0,1.5]} rotation={[0,0,0]}/>
         </group>
     )
 }
